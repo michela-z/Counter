0969c96daebca3f4659d395ddabda2c01bee8f4c
michela-z <54006438+michela-z@users.noreply.github.com>
2022-01-19 09:39:14 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+let btnAdd = document.querySelector('#btn-add');
+let btnSub = document.querySelector('#btn-sub');
+
+const counter = document.querySelector('#counter');
+
+btnAdd.addEventListener('click', addNumber);
+btnSub.addEventListener('click', subNumber);
+
+let i = 0;
+
+function addNumber() {
+    counter.innerHTML = ++i;
+}
+
+function subNumber() {
+    counter.innerHTML = --i;
+}
+
+let reset = document.querySelector('#reset');
+
+reset.addEventListener('click', function() {
+    i = 0;
+    counter.innerHTML = i;
+})
